3773ba1160da955b1288e2ac8e35c7ebc2af7a12
Walter Pinto <walterjp@google.com>
2024-10-03 17:54:32 -0400

    Removing deprecated APIs from Interop

--- RecaptchaEnterprise/RecaptchaInterop/RCIClientFactory.swift
+++ RecaptchaEnterprise/RecaptchaInterop/RCIClientFactory.swift
@@ -20,49 +20,7 @@ import Foundation
 
 /// Interface to interact with reCAPTCHA and retrieve a reCAPTCHA Client.
 public enum RCIClientFactory {
-  /// Builds a new reCAPTCHA Client for the given Site Key and timeout.
-  ///
-  /// The SDK currently supports one Site Key. Passing a different Site Key will
-  /// throw an exception.
-  ///
-  /// At least a 10000 millisecond timeout is suggested to allow for slow
-  /// networking, though in some cases longer timeouts may be necessary. The
-  /// minimum allowable value is 5000 milliseconds.
-  ///
-  /// - Parameters:
-  ///     - siteKey: reCAPTCHA Site Key for the app.
-  ///     - timeout: Timeout for getClient in milliseconds.
-  ///     - completion: Callback function to return the RecaptchaClient or an error.
-  @available(*, deprecated, message: "Use fetchClient api instead")
-  public static func getClient(withSiteKey siteKey: String, withTimeout timeout: Double,
-                               completion: @escaping (RCIClientProtocol?, Error?) -> Void) {
-    #if canImport(RecaptchaEnterprise)
-      Recaptcha.getClient(withSiteKey: siteKey, withTimeout: timeout, completion: completion)
-    #else
-      completion(nil, RCIError.recaptchaEnterpriseNotImported)
-    #endif
-  }
-
-  /// Builds a new reCAPTCHA Client for the given Site Key and timeout.
-  ///
-  /// The SDK currently supports one Site Key. Passing a different Site Key will
-  /// throw an exception.
-  ///
-  /// This method will throw a timeout exception after 10 seconds.
-  ///
-  /// - Parameters:
-  ///     - siteKey: reCAPTCHA Site Key for the app.
-  ///     - completion: Callback function to return the RCIClientProtocol or an error.
-  @available(*, deprecated, message: "Use fetchClient api instead")
-  public static func getClient(withSiteKey siteKey: String,
-                               completion: @escaping (RCIClientProtocol?, Error?) -> Void) {
-    #if canImport(RecaptchaEnterprise)
-      Recaptcha.getClient(withSiteKey: siteKey, completion: completion)
-    #else
-      completion(nil, RCIError.recaptchaEnterpriseNotImported)
-    #endif
-  }
-
+  
   /// Builds a new reCAPTCHA Client for the given Site Key.
   ///
   /// The SDK accepts one Site Key. Passing a different Site Key will throw an exception.
